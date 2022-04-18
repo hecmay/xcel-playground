f8ba331d5c2664f17a3355d9776d4b7522969d8c
Hecmay <sx233@cornell.edu>
2022-04-18 19:18:42 -0400

    first commit

---
+++ p2p/setup.sh
@@ -0,0 +1,8 @@
+
+function make_host {
+    /usr/local/cuda-11.3/bin/nvcc -o p2p_bandwidth xcl2.cpp cmdlineparser.cpp logger.cpp src/host.cpp -I/opt/xilinx/xrt/include -I/opt/xilinx/Xilinx_Vivado_vitis_2020.2/Vivado/2020.2/include -O0 -g -std=c++11 -I./ -L/opt/xilinx/xrt/lib -lOpenCL -lrt -lstdc++
+}
+
+function run_host {
+    ./p2p_bandwidth -x bandwidth.xclbin -p ./data/sample.txt -d 2
+}
